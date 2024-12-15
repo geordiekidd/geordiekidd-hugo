6d3543bf30be11cdc64273388a16f68fad4fff33
Geordie Kidd <geordiekidd@gmail.com>
2024-12-15 20:13:17 +1100

    Added title

--- static/js/tabactive.js
+++ static/js/tabactive.js
@@ -1,31 +1,3 @@
-// $(document).ready(function(){
-
-//   const placeValue = $("div[data-place]").attr("data-place");
-
-//   $("button[data-place=" + placeValue + "]").addClass("active");
-//   $("div[data-place=" + placeValue + "]").fadeIn(250, function(){
-//     $(this).addClass("active");
-//   });
-
-//   // Add fade-out effect on button click
-//   $("button").on("click", function(event) {
-//     const target = $(this).attr("href") || $(this).data("place-link"); // Adjust if your buttons have a custom data attribute for links
-//     if (target) {
-//       event.preventDefault(); // Prevent immediate redirection
-
-//       const $currentContent = $("div[data-place=" + placeValue + "]");
-
-//       const $currentButton = $("button[data-place=" + placeValue + "]");
-//       $currentButton.css("font-weight", "400"); // Update font weight to 400
-
-//       $currentContent.fadeOut(250, function() {
-//         // Redirect after fade-out completes
-//         window.location.href = target;
-//       });
-//     }
-//   });
-// });
-
 $(document).ready(function() {
   const placeValue = $("div[data-place]").attr("data-place");
 
@@ -42,7 +14,7 @@ $(document).ready(function() {
       event.preventDefault(); // Prevent immediate navigation
 
       const $currentContent = $("div[data-place=" + placeValue + "]");
-      
+     
       const $currentButton = $("button[data-place=" + placeValue + "]");
       $currentButton.css("font-weight", "400"); // Update font weight to 400
 
